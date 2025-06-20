3dfd9ae185f84d984a72105957e88b015c22404c
Joen Tan <joentze01@gmail.com>
2025-06-20 16:36:12 +0800

    feat: added typegen

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ const api = new Hono();
 api.basePath("/api");
 api.route("/projects", projects.default);
 
+export type HonoRoutes = typeof api;
 export default {
   port: 5174,
   fetch: api.fetch,
